1f6c1f5c3bc7b9c359ab0ce9bfd30c502b0bbe65
evgen <parom.evgen@mail.ru>
2023-04-15 17:32:55 +0300

    allow preflight request

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,8 +18,12 @@ public class BasicAuthenticationSecurityConfiguration {
     //stateless rest api
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        //1: response to preflight request doesn't pass access control check
+        //2: basic auth
+        http.authorizeHttpRequests(
+                auth -> auth
+                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyRequest().authenticated());
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf().disable();
