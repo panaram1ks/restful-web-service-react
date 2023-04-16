26b0c7addbbe1358d6f4c7dd1043dd80f6d4f012
evgen <parom.evgen@mail.ru>
2023-04-16 12:23:01 +0300

    Security Suffering!!!

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -1,14 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices.basic;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 public class BasicAuthenticationSecurityConfiguration {
 
     //Filter chain
@@ -16,18 +15,18 @@ public class BasicAuthenticationSecurityConfiguration {
     // basic authentication
     //disabling csrf
     //stateless rest api
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //1: response to preflight request doesn't pass access control check
-        //2: basic auth
-        http.authorizeHttpRequests(
-                auth -> auth
-                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .anyRequest().authenticated());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.csrf().disable();
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        //1: response to preflight request doesn't pass access control check
+//        //2: basic auth
+//        http.authorizeHttpRequests(
+//                auth -> auth
+//                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                        .anyRequest().authenticated());
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.csrf().disable();
+//
+//        return http.build();
+//    }
 }
